0f4b617158ed665d907aadf58d3e8749d93ae17e
Anusri Kadam <anusrikadam@gmail.com>
2025-04-16 09:52:40 +0530

    Create StudentOperations.java

---
+++ StudentOperations.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import exceptions.*;
+
+public class StudentOperations {
+    private ArrayList<Student> students;
+
+    public StudentOperations() {
+        students = new ArrayList<>();
+    }
