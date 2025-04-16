221a4b993a5b2efb08f0a74eaabbdab7a237d73b
Anusri Kadam <anusrikadam@gmail.com>
2025-04-16 10:01:24 +0530

    Create InvalidPositionException.java

---
+++ exceptions/InvalidPositionException.java
@@ -0,0 +1,8 @@
+//InvalidPositionException.java
+package exceptions;
+
+public class InvalidPositionException extends Exception {
+    public InvalidPositionException(String message) {
+        super(message);
+    }
+}
