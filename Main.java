55d3429a0fac20fad3a2dd92f63bd73123e0b30c
Anusri Kadam <anusrikadam@gmail.com>
2025-04-16 09:56:30 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,3 +6,14 @@ Batch: SY AIML A1
 
 import java.util.*;
 import exceptions.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StudentOperations operations = new StudentOperations();
+
+        int choice;
+        do {
+            System.out.println("1. Add Student\n2. Display All Students\n3. Search by PRN\n4. Search by Name");
+            System.out.println("5. Search by Position\n6. Update Student\n7. Delete Student\n8. Exit");
+            System.out.print("Enter your choice: ");
+            choice = Integer.parseInt(scan.nextLine());
