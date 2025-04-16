77af399b5a9a761d094f777df806f2a960f17300
Anusri Kadam <anusrikadam@gmail.com>
2025-04-16 09:48:56 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+//Student.java
+public class Student {
+    private String name;
+    private int prn;
+    private String branch;
+    private String batch;
+    private float cgpa;
+
+    // Constructor
+    public Student(String name, int prn, String branch, String batch, float cgpa) {
+        this.name = name;
+        this.prn = prn;
+        this.branch = branch;
+        this.batch = batch;
+        this.cgpa = cgpa;
+    }
